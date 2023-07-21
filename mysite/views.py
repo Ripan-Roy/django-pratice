b327597272a68a3eed3e78d1212d7b066121230c
ripan-roy <ripanroy111@gmail.com>
2023-07-22 02:37:17 +0530

    added views

---
+++ mysite/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def aboutUs(request):
+    return HttpResponse("You're at the about us page")
+
+def course(request):
+    return HttpResponse("You're at the course page.")
